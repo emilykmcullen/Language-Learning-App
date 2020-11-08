b95544552470d21b454c1bf40b2ccd62a987f796
emilykmcullen <emilycullen135@gmail.com>
2020-11-08 09:48:56 +0000

    Added a 'new phrase' form and page

--- controllers/first_language_phrase_controller.py
+++ controllers/first_language_phrase_controller.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect
 from flask import Blueprint
 from models.first_language_phrase import FirstLanguagePhrase
 import repositories.first_language_phrase_repository as first_language_phrase_repository
+import repositories.language_repository as language_repository
 
 first_language_phrases_blueprint = Blueprint("first_language_phrases", __name__)
 
@@ -27,4 +28,5 @@ def edit_phrase(id):
 
 @first_language_phrases_blueprint.route("/sentence_snaps/new")
 def new_phrase():
-    return render_template("sentence_snaps/new.html")
+    languages = language_repository.select_all()
+    return render_template("sentence_snaps/new.html", languages=languages)
