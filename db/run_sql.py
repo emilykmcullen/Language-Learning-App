36b5999143be2f1addc0df8abb2dc396cf306ad0
emilykmcullen <emilycullen135@gmail.com>
2021-01-03 11:47:17 +0000

    commented out line of code

--- db/run_sql.py
+++ db/run_sql.py
@@ -4,7 +4,7 @@ import os
 
 DATABASE_URL = os.environ['DATABASE_URL']
 
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 
 def run_sql(sql, values = None):
